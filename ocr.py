27fd0eb544c016d2e6adde6587c39e9cff44605c
thoma <thomashsu0512@gmail.com>
2024-09-04 01:30:38 -0400

    checks equal sign

--- ocr.py
+++ ocr.py
@@ -5,6 +5,29 @@ from model import process_image
 from PIL import Image
 
 
+def find_character(image, bottom, top, left, right):
+    # crop out the character image
+    char_image = image[bottom:top, left:right]
+
+    # calculate padding to square image
+    height, width = char_image.shape
+
+    if height > width:
+        top_padding = 0
+        bottom_padding = 0
+        left_padding = right_padding = (height - width) // 2
+    else:
+        left_padding = 0
+        right_padding = 0
+        top_padding = bottom_padding = (width - height) // 2
+
+    squared_image = cv2.copyMakeBorder(char_image, top_padding, bottom_padding, left_padding, right_padding,
+                                       cv2.BORDER_CONSTANT, value=255)
+
+    pil_image = Image.fromarray(squared_image)
+    return process_image(pil_image)
+
+
 def parse(image_path):
     image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
     threshold, binary_image = cv2.threshold(image, 128, 255, cv2.THRESH_BINARY_INV)
@@ -54,30 +77,16 @@ def parse(image_path):
     bottom = horizontal_bounds[0][0]
     top = horizontal_bounds[0][1]
 
+    # check if last character is an equal sign
+    last_character_idx = len(vertical_bounds) - 1
+    if find_character(image, bottom, top, vertical_bounds[last_character_idx][0], vertical_bounds[last_character_idx][1]) != '=':
+        return
+
     expression_string = ""
-
-    for i, (left, right) in enumerate(vertical_bounds):
-        # crop out the character image
-        char_image = image[bottom:top, left:right]
-
-        # calculate padding to square image
-        height, width = char_image.shape
-
-        if height > width:
-            top_padding = 0
-            bottom_padding = 0
-            left_padding = right_padding = (height - width) // 2
-        else:
-            left_padding = 0
-            right_padding = 0
-            top_padding = bottom_padding = (width - height) // 2
-
-        squared_image = cv2.copyMakeBorder(char_image, top_padding, bottom_padding, left_padding, right_padding,
-                                           cv2.BORDER_CONSTANT, value=255)
-        pil_image = Image.fromarray(squared_image)
-        expression_string += process_image(pil_image)
+    for (left, right) in vertical_bounds:
+        expression_string += find_character(image, bottom, top, left, right)
 
     return expression_string
 
 
-print(parse("test_images/img.png"))
+print(parse("test_images/img_1.png"))
